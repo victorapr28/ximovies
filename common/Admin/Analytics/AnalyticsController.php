d30532f0417603670e46e463a34deae9a7cf8bfc
Victor <victorapr28@gmail.com>
2020-06-24 19:04:21 -0700

    d

--- common/Admin/Analytics/AnalyticsController.php
+++ common/Admin/Analytics/AnalyticsController.php
@@ -3,6 +3,7 @@
 use Cache;
 use Carbon\Carbon;
 use Common\Core\BaseController;
+use Common\Admin\Analytics\Actions\GetAnalyticsData;
 use Common\Admin\Analytics\Actions\GetAnalyticsHeaderDataAction;
 use Exception;
 use Illuminate\Http\JsonResponse;
@@ -10,6 +11,11 @@ use Illuminate\Http\Request;
 
 class AnalyticsController extends BaseController
 {
+    /**
+     * @var GetAnalyticsData
+     */
+    private $getDataAction;
+
     /**
      * @var GetAnalyticsHeaderDataAction
      */
@@ -23,14 +29,17 @@ class AnalyticsController extends BaseController
     const DEFAULT_CHANNEL = 'default';
 
     /**
+     * @param GetAnalyticsData $getDataAction
      * @param GetAnalyticsHeaderDataAction $getHeaderDataAction
      * @param Request $request
      */
     public function __construct(
         Request $request,
+        GetAnalyticsData $getDataAction,
         GetAnalyticsHeaderDataAction $getHeaderDataAction
     )
     {
+        $this->getDataAction = $getDataAction;
         $this->getHeaderDataAction = $getHeaderDataAction;
         $this->request = $request;
     }
@@ -43,18 +52,14 @@ class AnalyticsController extends BaseController
         $this->authorize('index', 'ReportPolicy');
 
         $channel = $this->request->get('channel') ?: self::DEFAULT_CHANNEL;
-        $mainData = [
-            'weeklyPageViews' => [
-                'current' => [],
-                'previous' => [],
-            ],
-            'monthlyPageViews' => [
-                'current' => [],
-                'previous' => [],
-            ],
-            'browsers' => [],
-            'countries' => []
-        ];
+
+        $mainData = $data = Cache::remember("analytics.data.main.$channel", Carbon::now()->addDay(), function() use($channel) {
+            try {
+                return $this->getDataAction->execute($channel);
+            } catch (Exception $e) {
+                return null;
+            }
+        }) ?: [];
 
         $headerData = $data = Cache::remember("analytics.data.header.$channel", Carbon::now()->addDay(), function() use($channel) {
             return $this->getHeaderDataAction->execute($channel);
